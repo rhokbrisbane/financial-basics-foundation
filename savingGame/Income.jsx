285ab5182912d76450d3900b1430e1b8a59749d7
Bruce <wcbushy@gmail.com>
2016-11-26 16:07:46 +1000

    Progress for minigame

---
+++ savingGame/Income.jsx
@@ -0,0 +1,18 @@
+var React = require("react");
+
+var IncomeSheet = React.createClass({
+	getInitialState: function (char) {
+		return {
+			cash0: char.cash,
+			happy0: char.happiness,
+
+		}
+
+	}
+
+	render: function () {
+		<div class="row">
+		</div>
+	}
+
+};)
