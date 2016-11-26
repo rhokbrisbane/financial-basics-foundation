518e289060fb48e989e1b5f9feda129da9cf2326
Bruce <wcbushy@gmail.com>
2016-11-26 16:31:50 +1000

    Html Update

--- savingGame/Income.jsx
+++ savingGame/Income.jsx
@@ -10,9 +10,28 @@ var IncomeSheet = React.createClass({
 
 	}
 
-	render: function () {
+	render: function (char) {
+
+
+		return (
 		<div class="row">
+			<div class="col-xs-12">
+				<h1>Income and Expenses</h1>
+			</div>
 		</div>
+		<div class=row">
+			{this.makeIncome(char)}
+		</div>
+	)}
+
+	makeIncome: function (char) {
+		return (
+			$.each(, function (key, val) {
+				
+
+			})
+		)
+
 	}
 
 };)
