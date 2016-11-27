24aac8e64e5ccfa56ca873de8a41d3d2815afb62
smagin94 <sean.p.magin@gmail.com>
2016-11-27 14:43:10 +1000

    UI Tweaks

--- savingGame/src/js/Summary.jsx
+++ savingGame/src/js/Summary.jsx
@@ -11,22 +11,19 @@ var Summary = React.createClass({
         var {name, cash, happiness, income, expenses} = this.props.character;
 
         return (
-            <div className="panel panel-default">
-                <div className="panel-heading" role="button" data-toggle="collapse" data-target="#summary-panel" aria-expanded={true} aria-controls="#summary-panel" style={{display: "flex", justifyContent: "space-between"}}>
-                    <h3 className="panel-title">
-                        Summary
-                    </h3>
-                    <h3 className="panel-title">
+            <div className="panel panel-primary">
+                <div className="panel-heading" role="button" data-toggle="collapse" data-target="#summary-panel" aria-expanded={true} aria-controls="#summary-panel">
+                    <h3 className="text center panel-title">
                         Week: {this.props.turn}
                     </h3>
                 </div>
-                <div id="summary-panel" className="panel-collapse collapse in">
+                <div id="summary-panel" className="text-center panel-collapse collapse in">
                     <div className="panel-body">
-                        <div>Name: {name}</div>
-                        <div>Cash: ${helpers.printNumber(Math.round(cash * 100) / 100)}</div>
-                        <div>Happiness: {happiness >= 0 ? "+" : ""}{helpers.printNumber(happiness)}</div>
-                        <div>Income: ${helpers.printNumber(income)}</div>
-                        <div>Expenses: ${helpers.printNumber(expenses)}</div>
+                        <div style{{fontWeight: 700, fontSize: 24}}>{name}</div>
+                        <div><i class="fa fa-money" aria-hidden="true"></i>Cash: ${helpers.printNumber(Math.round(cash * 100) / 100)}</div>
+                        <div><i class="fa fa-arrow-smile-o" aria-hidden="true"></i>Happiness: {happiness >= 0 ? "+" : ""}{helpers.printNumber(happiness)}</div>
+                        <div><i class="fa fa-arrow-up" aria-hidden="true"></i>Income: ${helpers.printNumber(income)}</div>
+                        <div><i class="fa fa-arrow-down" aria-hidden="true"></i>Expenses: ${helpers.printNumber(expenses)}</div>
                     </div>
                 </div>
             </div>
