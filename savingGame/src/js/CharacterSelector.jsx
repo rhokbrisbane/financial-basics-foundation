45d079e747fb4d08055315cadb472a729ca98185
Ryan Ballinger <ryan.a.ballinger@gmail.com>
2016-11-27 13:11:51 +1000

    Removed tony stark :O :(

--- savingGame/src/js/CharacterSelector.jsx
+++ savingGame/src/js/CharacterSelector.jsx
@@ -5,7 +5,7 @@ var CharacterSelector = React.createClass({
     characters: [
         {name: "Reggie McRib", cash: 0, happiness: 0, happinessDecay: 10, income: 900, expenses: 400, minimumHappiness: -50, goal: "Car"},
         {name: "Sammie Sandwich", cash: 0, happiness: 0, happinessDecay: 5, income: 700, expenses: 250, minimumHappiness: -100, goal: "Car"},
-        {name: "Tony Stark", cash: 0, happiness: 0, happinessDecay: 5, income: 10000, expenses: 9200, minimumHappiness: -10, goal: "Awesome Car"},
+        //{name: "Tony Stark", cash: 0, happiness: 0, happinessDecay: 5, income: 10000, expenses: 9200, minimumHappiness: -10, goal: "Awesome Car"},
         {name: "Party Dude", cash: 0, happiness: 0, happinessDecay: 10, income: 1000, expenses: 300, minimumHappiness: -30, goal: "Weekend Away"}
     ],
 
